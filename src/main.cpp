f4622a9a6f43194b0a47040b2e5986ca5cd0b9cf
CleitonS <cleiton1302@gmail.com>
2018-06-14 23:33:10 -0300

    up

--- src/main.cpp
+++ src/main.cpp
@@ -560,7 +560,7 @@ OBS: COMENTEI PARA TESTES!!!
         // Desenhamos o modelo do Fundo
         model = Matrix_Translate(0.0f, -8.0f,0.0f)
                 * Matrix_Scale(6.0f, 0.0f, 6.0f)
-				* Matrix_Rotation(90) ;
+				* Matrix_Rotate_X(90);
         glUniformMatrix4fv(model_uniform, 1 , GL_FALSE , glm::value_ptr(model));
         glUniform1i(object_id_uniform, BACK);
         DrawVirtualObject("plane");
